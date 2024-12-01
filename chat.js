94ea38694798f926677b4beb0799ecf11a5a6399
adul <abdul.azizalrasyid@bssn.go.id>
2024-12-01 20:38:50 +0700

    finish the task

--- chat.js
+++ chat.js
@@ -1,17 +1,31 @@
 import dotenv from "dotenv";
-import { GoogleGenerativeAI } from "@google/generative-ai";
-
 dotenv.config();
+import * as fs from "fs";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
+function fileToGenerativePart(path, mimeType) {
+    return {
+        inlineData: {
+            data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+            mimeType,
+        },
+    };
+}
+
 async function run() {
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
     const prompt =
-        "Write a sonnet about a President University student life that take Machine Learning class, but also make it rhyme.";
+        "What is the difference between the two images? Understand whats written on the paper.";
 
-    const result = await model.generateContent(prompt);
+    const imageParts = [
+        fileToGenerativePart("pen.jpg", "image/jpeg"),
+        fileToGenerativePart("page-and-pen.jpg", "image/jpeg"),
+    ];
+
+    const result = await model.generateContent([prompt, ...imageParts]);
     const response = await result.response;
     const text = response.text();
     console.log(text);
